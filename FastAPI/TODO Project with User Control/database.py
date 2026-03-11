884eee6cc313f6c6bb6d6768a065af591909f9c8
Masum Mushfiq Ishti <mushfiq.ishti@gmail.com>
2026-03-12 00:24:35 +0600

    Add files via upload

---
+++ FastAPI/TODO Project with User Control/database.py
@@ -0,0 +1,12 @@
+#using SQLITE at first
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///todosapp.db' #creating a location on the fastAPI application to store the database
+#todos app db wll have 2 tables. One for user another for todo lis
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False}) #creating the database engine which is used to openup connection and close
+#allowing only one thread to connect for now
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) #session local will become actual database in the future
+Base= declarative_base() #object of a database model, which will be used to create tables in the database
